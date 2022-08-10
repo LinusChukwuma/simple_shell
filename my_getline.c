c2a7244845740dad7f515ef6b45c41c6d7929a6e
LinusChukwuma <104623011+LinusChukwuma@users.noreply.github.com>
2022-08-10 15:57:56 -0700

    Add files via upload

--- my_getline.c
+++ my_getline.c
@@ -1,144 +1,101 @@
-#include "main.h"
-
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b);
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
-
+#include "shell.h"
 /**
- * _realloc - Reallocates a memory block using malloc and free.
- * @ptr: A pointer to the memory previously allocated.
- * @old_size: The size in bytes of the allocated space for ptr.
- * @new_size: The size in bytes for the new memory block.
- *
- * Return: If new_size == old_size - ptr.
- *         If new_size == 0 and ptr is not NULL - NULL.
- *         Otherwise - a pointer to the reallocated memory block.
+ * _getline - gets a line from fd or std input
+ * @lineptr: buffer to fill line with
+ * @fd: file descriptor
+ * Return: num of characters
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+int _getline(char **lineptr, int fd)
 {
-	void *mem;
-	char *ptr_copy, *filler;
-	unsigned int index;
+	int size = 1025;
+	int old_size = 0;
+	int r = 1;
+	int sum = 0;
+	static char buffer[1025];
+	static int begin;
+	static int end;
+	int c = 0;
+	int d;
 
-	if (new_size == old_size)
-		return (ptr);
-
-	if (ptr == NULL)
+	if (fd == -2)
 	{
-		mem = malloc(new_size);
-		if (mem == NULL)
-			return (NULL);
-
-		return (mem);
+		begin = 0;
+		end = 0;
 	}
 
-	if (new_size == 0 && ptr != NULL)
+	if (lineptr == NULL)
 	{
-		free(ptr);
-		return (NULL);
+		return (0);
 	}
-
-	ptr_copy = ptr;
-	mem = malloc(sizeof(*ptr_copy) * new_size);
-	if (mem == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
-	filler = mem;
-
-	for (index = 0; index < old_size && index < new_size; index++)
-		filler[index] = *ptr_copy++;
-
-	free(ptr);
-	return (mem);
-}
-
-/**
- * assign_lineptr - Reassigns the lineptr variable for _getline.
- * @lineptr: A buffer to store an input string.
- * @n: The size of lineptr.
- * @buffer: The string to assign to lineptr.
- * @b: The size of buffer.
- */
-void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
-{
 	if (*lineptr == NULL)
 	{
-		if (b > 120)
-			*n = b;
-		else
-			*n = 120;
-		*lineptr = buffer;
-	}
-	else if (*n < b)
-	{
-		if (b > 120)
-			*n = b;
-		else
-			*n = 120;
-		*lineptr = buffer;
-	}
-	else
-	{
-		_strcpy(*lineptr, buffer);
-		free(buffer);
-	}
-}
-
-/**
- * _getline - Reads input from a stream.
- * @lineptr: A buffer to store the input.
- * @n: The size of lineptr.
- * @stream: The stream to read from.
- *
- * Return: The number of bytes read.
- */
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
-{
-	static ssize_t input;
-	ssize_t ret;
-	char c = 'x', *buffer;
-	int r;
-
-	if (input == 0)
-		fflush(stream);
-	else
-		return (-1);
-	input = 0;
-
-	buffer = malloc(sizeof(char) * 120);
-	if (!buffer)
-		return (-1);
-
-	while (c != '\n')
-	{
-		r = read(STDIN_FILENO, &c, 1);
-		if (r == -1 || (r == 0 && input == 0))
-		{
-			free(buffer);
+		*lineptr = malloc(sizeof(char) * size + 1);
+		if (*lineptr == NULL)
 			return (-1);
-		}
-		if (r == 0 && input != 0)
-		{
-			input++;
-			break;
-		}
-
-		if (input >= 120)
-			buffer = _realloc(buffer, input, input + 1);
-
-		buffer[input] = c;
-		input++;
 	}
-	buffer[input] = '\0';
 
-	assign_lineptr(lineptr, n, buffer, input);
-
-	ret = input;
-	if (r != 0)
-		input = 0;
-	return (ret);
+	while (1)
+	{
+		if (begin == end)
+		{
+			while (sum < 1024 && r != 0)
+			{
+				r = read(fd, buffer + sum, 1024 - sum);
+				begin = 0;
+				sum += r;
+				end = sum;
+				/*printf("r : %d\n", r);*/
+				for (d = 0; r != 0 && d < end; d++)
+					if (buffer[d] == '\n')
+						r = 0;
+			}
+			if (sum == 0)
+			{
+				buffer[0] = 0;
+				return (sum);
+			}
+			buffer[sum] = 0;
+			sum = 0;
+		}
+		for (; buffer[begin]; begin++)
+		{
+			if (begin == 1024)
+			{
+				/*free(buffer);*/
+				/*(*lineptr)[c] = EOF;*/
+				/*return (c);*/
+				break;
+			}
+			/*printf("beginning for\n");//debug check*/
+			if (buffer[begin] == '\n')
+			{
+				(*lineptr)[c] = '\n';
+				begin++;
+				c++;
+				(*lineptr)[c] = '\0';
+				return (c);
+			}
+			else
+			{
+				(*lineptr)[c] = buffer[begin];
+			}
+			c++;
+		}
+		if (c + begin >= 1024)
+		{
+			old_size = size;
+			size = size + 1024;
+			*lineptr = _realloc(*lineptr, old_size, size);
+			if (*lineptr == NULL)
+			{
+				return (-1);
+			}
+		}
+		else
+		{
+			(*lineptr)[old_size + begin] = 0;
+			return (c);
+		}
+		/*printf("j: %d, i:%d, r:%d\n", j, i ,r);*/
+	}
 }
-
